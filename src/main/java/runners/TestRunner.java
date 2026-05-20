7fa7b8c33f419e2c7f46b9e10b71412e1928d96c
Zehra Aykan <139782347+aykan001@users.noreply.github.com>
2026-05-20 12:42:20 +0300

    Update TestRunner.java

--- src/main/java/runners/TestRunner.java
+++ src/main/java/runners/TestRunner.java
@@ -1,4 +1,16 @@
 package runners;
 
-public class TestRunner {
+import io.appium.java_client.AppiumDriver;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import utils.DriverFactory;
+
+@CucumberOptions(
+        features = {"src/test/resources/features"},
+        glue = {"stepdefinitions","util"},
+        tags = "",
+        plugin = {"summary","pretty","html:reports/cucumberReport/Report.html"}
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+    static AppiumDriver driver= DriverFactory.getDriver();
 }
